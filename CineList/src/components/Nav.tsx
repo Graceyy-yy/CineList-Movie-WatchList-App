22e5ed8cbc8ff44676397fa590a163a004ee416d
Grace Kangausaru <Grace.Kangausaru@altron.com>
2026-04-07 16:04:06 +0200

    Fixed tailwind and added to Nav

--- CineList/src/components/Nav.tsx
+++ CineList/src/components/Nav.tsx
@@ -1,7 +1,16 @@
+import { TiThMenuOutline } from "react-icons/ti"; 
 import React from 'react'
+import Logo from "../assets/icon.svg"
 
 export const Nav = () => {
   return (
-    <div>Nav</div>
+    <div className = 'navbody'>
+      <img src = {Logo} width = {80} />
+      <h1  className='h1'>CineList</h1>
+      <button className = 'btn'>
+        <TiThMenuOutline className = 'text-green-600 text-4xl'/>
+      </button>
+
+    </div>
   )
 }
---
+++ CineList/tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [],
+  theme: {
+    extend: {
+      colors: {
+        navy: '#0a192f'
+      }
+    },
+  },
+  plugins: [],
+}
+
